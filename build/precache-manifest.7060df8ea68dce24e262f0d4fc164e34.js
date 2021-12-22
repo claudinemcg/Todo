b8c8d34659b5379826acb16c44df923fa6fc4982
Claudine McGowan <claudine.mcgowan@gmail.com>
2021-12-22 16:43:05 +0000

    last changes before deploying

---
+++ build/precache-manifest.7060df8ea68dce24e262f0d4fc164e34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b11e50b7344e547ba65f",
+    "url": "/static/css/main.88152d36.chunk.css"
+  },
+  {
+    "revision": "b11e50b7344e547ba65f",
+    "url": "/static/js/main.c728a10b.chunk.js"
+  },
+  {
+    "revision": "8dd6e0312f6750e3dec4",
+    "url": "/static/js/runtime~main.4ce16dc0.js"
+  },
+  {
+    "revision": "b356dafac80ef94ec0f3",
+    "url": "/static/js/2.5144bb3a.chunk.js"
+  },
+  {
+    "revision": "69479d3d7550ac7572aa",
+    "url": "/static/js/3.0711f24a.chunk.js"
+  },
+  {
+    "revision": "997df78772a728bbf8cfb15256cfae81",
+    "url": "/index.html"
+  }
+];
